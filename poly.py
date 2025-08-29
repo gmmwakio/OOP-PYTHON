c1bfe3e8f937dcdd6ef6efe685d79535c76e63fe
George Mwakio <mwakiogeorge90@gmail.com>
2025-08-29 12:46:02 +0300

    Add files via upload

---
+++ poly.py
@@ -0,0 +1,16 @@
+class Dog:
+    def __init__(self):
+        print("Dog walking")
+
+class Bird:
+    def __init__(self):
+        print("Bird Flying ")
+
+class Fish:
+    def __init__(self):
+        print("Fish swimming")
+
+d1 = Dog()
+b1 = Bird()
+f1 = Fish()
+
